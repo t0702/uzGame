efd8c3a7e4a0bd9deb70f608a44a1804b3a94598
t0702 <t0702@naver.com>
2020-03-30 06:09:01 +0900

    adsf

--- BreakOut/js/game.js
+++ BreakOut/js/game.js
@@ -130,11 +130,58 @@ function ballPaddleCollision(){
     }
 }
 
+//create the bricks
+const brick = {
+    row : 3,
+    column : 5,
+    width : 55,
+    height : 20,
+    offSetLeft : 20,
+    offSetTop : 40,
+    marginTop : 50,
+    fillColor : '#2e3548',
+    strokeColor : '#FFF'
+}
+
+let bricks = [];
+function creatBricks(){
+    for (let r = 0; r < brick.row; r++){
+        bricks[r] = [];
+        for (let c = 0; c < brick.column; c++){
+            bricks[r][c] = {
+                x : c * ( brick.offSetLeft + brick.width ) + brick.offSetLeft,
+                y : r * ( brick.offSetTop + brick.height ) +brick.offSetTop + brick.marginTop,
+                status : true
+            }
+        }
+    }
+}
+
+creatBricks();
+
+//draw the bricks
+function drawBricks(){
+    for (let r = 0; r < brick.row; r++){
+        for (let c = 0; c < brick.column; c++){
+            let b = bricks[r][c];
+            //if the brick isn't brocken
+            if(b.status){
+                ctx.fillStyle = brick.fillColor;
+                ctx.fillRect(b.x, b.y, brick.width, brick.height);
+
+                ctx.storkeStyle = brick.strokeColor;
+                ctx.strokeRect(b.x, b.y, brick.width, brick.height);
+            }
+        }
+    }
+}
 //draw function
 function draw(){
     drawPaddle();
 
     drawBall();
+
+    drawBricks();
 }
 
 //update game function
